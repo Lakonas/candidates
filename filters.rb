d692dc22a7cb7796fc7b86830c5fa04967509d7a
Bill Katsoulis <bill.katsoulis@rogers.com>
2025-05-09 00:05:15 -0400

    candidate filters

--- filters.rb
+++ filters.rb
@@ -14,3 +14,30 @@ def find(id)
   end
   
   # More methods will go below
+
+  def experienced?(candidate)
+    candidate[:years_of_experience] >= 2
+  end
+
+  def find(id)
+    @candidates.find { |candidate| candidate[:id] == id }
+  end
+
+  def qualified_candidates(candidates)
+    candidates.select do |candidate|
+      experienced?(candidate) &&
+      candidate[:github_points] >= 100 &&
+      (candidate[:languages].include?("Ruby") || candidate[:languages].include?("Python")) &&
+      candidate[:date_applied] >= 15.days.ago.to_date &&
+      candidate[:age] >= 18
+    end
+  end
+
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+  end
+  
+  
+  
+  
+
